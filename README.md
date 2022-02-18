bffc08bd88a2e768c820ee1527b7b3d549930a4d
James Johnston <jejohnston@outlook.com>
2022-02-17 21:50:02 -0800

    Minor refax/cleanup, added instructions.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # choochbot-premium
+
+Instructions for Ryan/Danny:
+
+1. Clone this repository from github. See instructions:
+https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository
+
+2. Install NodeJS and NPM (node package manager) if not installed. See instructions:
+https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+
+3. Run 'npm install' in the working directory. This installs your dependencies. 
+
+4. To run choochbot, run the following command in a terminal. Change the WALLET and ENDPOINT values to reflect
+the address of the ethermine wallet and the discord endpoint you want to hit. Discord endpoint can be found
+in our server settings.
+
+WALLET=0x123456... ENDPOINT=https://discord.com/api/webhooks/ node index.js
+
+5. To run it regularly, use a cron job on linux or a scheduled task on Windows. I can help you with setting up a
+cron job but you'll have to ask Kyle or someone how to set it up on Windows.
--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const axios = require('axios');
-const Papa = require('papaparse');
-const fs = require('fs');
 
-let config = {};
-let startedAt, currentProgressPct, currentProgressEth, activeWorkers, hashrate, walletBalanceEth, 
+let currentProgressPct, activeWorkers, hashrate, walletBalanceEth, 
 currentSharePrice, walletBalanceUSD, ethUSDPrice, slowGas, stdGas, fastGas, rapidGas;
 let progressSinceLastExecution = 0;
 let progressDelta = 0;
-let historicalData = [];
 
-const CONFIG_FILE = process.env.CONFIG_FILE || 'config.json';
-const HIST_FILE = process.env.HIST_FILE || 'historicaldata.csv';
-const MAX_HIST_DATA = process.env.MAX_HIST_DATA || 1000;
+const WALLET = process.env.WALLET || '';
+const ENDPOINT = process.env.ENDPOINT || '';
+const NUM_CHOOCHERS = process.env.NUM_CHOOCHERS || 3;
 
-async function main() {
-  startedAt = new Date().toString();
-  console.log(`Beginning execution at: ${startedAt}`);
+exports.handler = async (event) => {
+  console.log(`Beginning execution at: ${new Date()}`);
 
-  //Load the config values from JSON.
-  await loadConfig();
+  if(!WALLET) {
+    throw new Error(`Wallet env undefined`);
+  }
+
+  if(!ENDPOINT) {
+    throw new Error(`Endpoint env undefined`)
+  }
 
   //Build out the current data set.
-  await buildCurrentDataset();
-
-  //Write current data to local file storage.
-  await writeCurrentData();
+  await buildDataset();
 
   //Output data to discord.
   await outputToDiscord();
@@ -33,7 +30,7 @@ async function main() {
 /**
  * Wrapper 
  */
-async function buildCurrentDataset() {
+async function buildDataset() {
   await buildEthermine();
   await buildGasData();
   await buildWalletBalance();
@@ -43,7 +40,7 @@ async function buildCurrentDataset() {
  * Hits the ethermine endpoint to get current miner statistics.
  */
 async function buildEthermine() {
-  let endpoint = `https://api.ethermine.org/miner/${config.WALLET}/dashboard`
+  let endpoint = `https://api.ethermine.org/miner/${WALLET}/dashboard`
   let res = await axios.get(endpoint);
   let currentStats = res.data.data.currentStatistics;
   activeWorkers = currentStats.activeWorkers;
@@ -60,7 +57,8 @@ async function buildEthermine() {
  */
 async function buildGasData() {
   try {
-  let endpoint = `https://www.gasnow.org/api/v3/gas/price?utm_source=:xx`;
+  let endpoint = `https://etherchain.org/api/gasnow`;
+  
   let res = await axios.get(endpoint);
   //Round off the values.
   rapidGas = (res.data.data.rapid / 1000000000).toPrecision(3);
@@ -80,53 +78,12 @@ async function buildGasData() {
  * Hits EthExplorer API to get wallet balance and Eth -> USD Rates.
  */
 async function buildWalletBalance() {
-  let endpoint = `http://api.ethplorer.io/getAddressInfo/${config.WALLET}?apiKey=freekey`
+  let endpoint = `http://api.ethplorer.io/getAddressInfo/${WALLET}?apiKey=freekey`
   let res = await axios.get(endpoint);
   walletBalanceEth = res.data.ETH.balance.toFixed(5);
   ethUSDPrice = res.data.ETH.price.rate.toFixed(2);
   walletBalanceUSD = (walletBalanceEth * ethUSDPrice).toFixed(2);
-  currentSharePrice = (walletBalanceUSD / 5).toFixed(2);
-}
-
-/**
- * Loads sensitive config values from JSON.
- * @returns 
- */
-async function loadConfig() {
-  let rawData = fs.readFileSync(`${__dirname}/${CONFIG_FILE}`, 'utf-8');
-  config = JSON.parse(rawData);
-}
-
-/**
- * Loads historical data from local file storage.
- */
-async function loadHistoricalData() {
-  let file = fs.readFileSync(HIST_FILE, 'utf-8');
-  let histData = Papa.parse(file, { header: true });
-  historicalData = histData.data;
-}
-
-/**
- * Writes the current data to the CSV file.
- */
-async function writeCurrentData() {
-  //Make sure we don't fill up my HDD.
-  if (historicalData.length >= MAX_HIST_DATA) {
-    //@todo -> Write to archive?
-    historicalData.shift();
-  }
-
-  historicalData.push({
-    'timestamp': startedAt,
-    'hashrate': hashrate,
-    'walletBalanceEth': walletBalanceEth,
-    'walletBalanceUSD': walletBalanceUSD,
-    'progresspct': currentProgressPct,
-    'progresseth': currentProgressEth,
-    'progressSinceLastExecution': progressSinceLastExecution
-  });
-  let outputCSV = Papa.unparse(historicalData, { header: true });
-  fs.writeFileSync(HIST_FILE, outputCSV);
+  currentSharePrice = (walletBalanceUSD / NUM_CHOOCHERS).toFixed(2);
 }
 
 /**
@@ -138,8 +95,7 @@ async function outputToDiscord() {
     "embeds": [
       {
         "title": "Ethermine Dashboard",
-        "url": `https://ethermine.org/miners/${config.WALLET}/dashboard`,
-        //"color": 15258703,
+        "url": `https://ethermine.org/miners/${WALLET}/dashboard`,
         "fields": [
           {
             "name": "Current progress",
@@ -189,26 +145,10 @@ async function outputToDiscord() {
     })
   }
 
-  await axios.post(config.DISCORD_ENDPOINT_PROD, data);
+  await axios.post(ENDPOINT, data);
 }
 
-/**
- * Determine how much of a percentage gain/loss we've had over the last interval.
- */
-async function buildComputedProps() {
-  if (historicalData.length > 0) {
-    //Get where we were at with the last execution.
-    let lastProgress = historicalData[historicalData.length - 1].progresspct;
-    //Get the delta
-    progressSinceLastExecution = (Number(currentProgressPct) - Number(lastProgress)).toPrecision(1);
-    
-    let lastDelta = historicalData[historicalData.length - 1].progressSinceLastExecution;
-    progressDelta = (progressSinceLastExecution - lastDelta).toPrecision(1);
-  }
-}
 
-// main().then(res => {
-//   console.log(`Finished with: ${res}`);;
-// }).catch(e => {
-//   console.log(`Found error: ${JSON.stringify(e.message)}`);
-// })
+this.handler().then(res => {
+  console.log(res)
+})
